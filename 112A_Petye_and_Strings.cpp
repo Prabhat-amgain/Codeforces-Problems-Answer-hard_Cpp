751fd5700582ea876bc16f5d38d93a892b126c2b
Prabhat Amgain <amgainparbhat@gmail.com>
2025-03-15 22:40:55 +0545

    Update 112A_Petye_and_Strings.cpp

--- 112A_Petye_and_Strings.cpp
+++ 112A_Petye_and_Strings.cpp
@@ -9,12 +9,12 @@ Now Petya wants to compare those two strings lexicographically.
 The letters' case does not matter, that is an uppercase letter is considered equivalent to the
 corresponding lowercase letter. Help Petya perform the comparison.
 
-Input
+Input:
 Each of the first two lines contains a bought string.
 The strings' lengths range from 1 to 100 inclusive. It is guaranteed that 
 the strings are of the same length and also consist of uppercase and lowercase Latin letters.
 
-Output
+Output:
 If the first string is less than the second one, print "-1". 
 If the second string is less than the first one, print "1". 
 If the strings are equal, print "0".
