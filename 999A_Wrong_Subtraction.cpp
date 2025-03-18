9b7244ea46bc8db52be6a7b5ca5241420ceb152f
Prabhat Amgain <amgainparbhat@gmail.com>
2025-03-18 19:10:41 +0545

    Add files via upload

---
+++ 999A_Wrong_Subtraction.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int i,j;
+    long int n;
+	int k;
+    cin>>n>>k;
+  for(i=0;i<k;i++){
+  
+    if(n%10==0){
+    	n=n/10;
+	}
+	else{
+		n-=1;
+	}
+}
+ cout<<n;
+}
