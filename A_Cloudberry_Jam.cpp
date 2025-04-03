5bf99edfb7773e2e15943fdaa60f26aded67af47
Prabhat Amgain <amgainparbhat@gmail.com>
2025-04-03 22:43:52 +0545

    Add files via upload

---
+++ A_Cloudberry_Jam.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t,n,i,j;
+void solve(){
+  cin>>n;
+  	cout<<2*n<<endl;
+}
+int main(){
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin>>t;
+    while(t--){
+    	solve();
+	}
+}
+
+
