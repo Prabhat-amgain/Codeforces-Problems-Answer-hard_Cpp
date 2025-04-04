e5ae45327716995117d714da7f8b32b1c41417ff
Prabhat Amgain <amgainparbhat@gmail.com>
2025-04-04 18:51:45 +0545

    Add files via upload

---
+++ 1999A_A+B_Again.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t,n,i,j;
+ void solve(){
+ 	cin>>n;		
+ 		int x = n%10;
+ 		int m = n/10;
+ 		int  y= m+x;
+ 		cout<<y<<endl;
+	 
+ }
+int main(){
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+  cin>>t;
+  while(t--){
+  	solve();
+  }
+
+
+}
+
+
