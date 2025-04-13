97d09f8b0d3c0c1cd82f61765110fca87445e02f
Prabhat Amgain <amgainparbhat@gmail.com>
2025-04-13 19:45:50 +0545

    Add files via upload

---
+++ 2044_Easy_Problem.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t,n,i,j;
+ void solve(){
+ 	int k;
+ 	cin>>n;
+ 	cout<<n-1<<endl;
+ }
+int main(){
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin>>t;
+    while(t--)
+    {
+    	solve();
+	}
+
+
+}
+
+
