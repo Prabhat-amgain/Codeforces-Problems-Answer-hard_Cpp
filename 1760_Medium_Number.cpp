042f4ea9bd120e1849e1579a8436b6f49c732d9d
Prabhat Amgain <amgainparbhat@gmail.com>
2025-04-23 23:19:26 +0545

    Update 1760_Medium_Number.cpp

--- 1760_Medium_Number.cpp
+++ 1760_Medium_Number.cpp
@@ -1,35 +1,19 @@
-/*A. Medium Number
-time limit per test1 second
-memory limit per test256 megabytes
-Given three distinct integers a
-, b
-, and c
-, find the medium number between all of them.
-
+/*                                                                  A. Medium Number
+                                                                                                             time limit per test 1 second
+                                                                                                             memory limit per test 256 megabytes
+Given three distinct integers a, b, and c, find the medium number between all of them.
 The medium number is the number that is neither the minimum nor the maximum of the given three numbers.
-
-For example, the median of 5,2,6
- is 5
-, since the minimum is 2
- and the maximum is 6
-.
+For example, the median of 5,2,6 is 5, since the minimum is 2 and the maximum is 6.
 
 Input
-The first line contains a single integer t
- (1≤t≤6840
-) — the number of test cases.
-
-The description of each test case consists of three distinct integers a
-, b
-, c
- (1≤a,b,c≤20
-).
+The first line contains a single integer t (1≤t≤6840) — the number of test cases.
+The description of each test case consists of three distinct integers a, b, c (1≤a,b,c≤20).
 
 Output
 For each test case, output a single integer — the medium number of the three numbers.
 
 Example
-InputCopy
+Input:
 9
 5 2 6
 14 3 4
@@ -40,7 +24,7 @@ InputCopy
 6 20 3
 4 1 3
 19 8 4
-OutputCopy
+Output:
 5
 4
 2
@@ -50,10 +34,7 @@ OutputCopy
 6
 3
 8
-
-
-
-    */
+                                                                                                               */
 #include <iostream>
 #include <algorithm>
 using namespace std;
